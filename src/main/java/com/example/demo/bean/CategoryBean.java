7e0422e61f637c4473a1b9673fb0b5271c0d8db6
Strawberry <honeykokia@gmail.com>
2025-03-31 14:29:08 +0800

    debug

---
+++ src/main/java/com/example/demo/bean/CategoryBean.java
@@ -0,0 +1,32 @@
+package com.example.demo.bean;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name="category")
+public class CategoryBean {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name="type" , nullable = false)
+    private byte type;
+
+}
