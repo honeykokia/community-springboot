94a3c9b25cb9fe876af285f014744fcba579cf10
honeykokia <72547838+honeykokia@users.noreply.github.com>
2025-05-24 13:42:06 +0800

    Dev (#6)
    * add properties test
    
    * test pr
    
    * debuge 掛載volume

--- src/main/java/com/example/demo/utils/PasswordVaildator.java
+++ src/main/java/com/example/demo/utils/PasswordVaildator.java
@@ -1,7 +1,7 @@
 package com.example.demo.utils;
 
 public class PasswordVaildator {
-    private static final String PASSWORD_PATTERN="^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[A-Za-z\\d]{6,12}$";
+    private static final String PASSWORD_PATTERN="^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[A-Za-z\\d]{8,12}$";
 
     public static boolean isValidPassword(String password) {
         return password.matches(PASSWORD_PATTERN);
