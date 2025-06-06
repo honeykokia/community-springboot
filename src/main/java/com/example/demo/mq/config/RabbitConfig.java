37f97a4680ebc8c0f4212ab03a9a57ab68e1b4d3
honeykokia <72547838+honeykokia@users.noreply.github.com>
2025-06-07 04:59:32 +0800

    debug RabbitMQ Message序列化失敗
    * add BCryptPasswordEncoder 加密方式
    
    * 修正 驗證錯誤邏輯，新增ErrorResult、ValidationResult
    
    * AccountController 權責整理
    
    * Record Controller 權責修正
    
    * debug 加密邏輯錯誤
    
    * 刪除舊代碼 ValidationResultOld
    
    * 刪除舊代碼 ValidationResultOld
    
    * AccountControllerTest Debug
    
    * 新增api流量攔截器 防止大量請求
    
    * redis cache debug
    
    * debug Redis cache
    
    * debug birthday欄位檢查錯誤、新增mq
    
    * debug rabbitmq
    
    * 新增api流量攔截器 防止大量請求
    
    * redis cache debug
    
    * debug Redis cache
    
    * debug birthday欄位檢查錯誤、新增mq
    
    * debug rabbitmq
    
    * debug ControllerTest
    
    * debug RabbitMQ Message序列化失敗

--- src/main/java/com/example/demo/mq/config/RabbitConfig.java
+++ src/main/java/com/example/demo/mq/config/RabbitConfig.java
@@ -1,9 +1,14 @@
 package com.example.demo.mq.config;
 
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -23,6 +28,18 @@ public class RabbitConfig {
     @Bean
     public Binding binding(Queue queue, DirectExchange exchange) {
         return BindingBuilder.bind(queue).to(exchange).with("user.sendmail");
+    }
+
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public AmqpTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter());
+        return template;
     }
 
 }
